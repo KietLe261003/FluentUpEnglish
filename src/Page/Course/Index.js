6f27ffb418c6c1e201d3dc96d0cdec9fb9fc2e39
KietLe <letuankiet26102003kl@gmail.com>
2024-12-04 16:30:22 +0700

    done

--- src/Page/Course/Index.js
+++ src/Page/Course/Index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Course.css';
 import { Outlet } from 'react-router-dom';
+import ListCard from '../../Components/Course/ListCard';
 function Course() {
     
     return (
         <div>
+            <ListCard></ListCard>
             <Outlet/>
         </div>
     );
