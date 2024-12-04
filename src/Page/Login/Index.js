c59838fe0f62ac7f5d2816aee6b719a49c48321a
KietLe <letuankiet26102003kl@gmail.com>
2024-12-04 15:36:51 +0700

    done login

--- src/Page/Login/Index.js
+++ src/Page/Login/Index.js
@@ -6,13 +6,13 @@ import ListIcon from '../../../src/Components/Login/ListIcon';
 import { Link } from "react-router-dom";
 function Login() {
     return (
-        <section class="vh-100 gradient-custom">
+        <section class="gradient-custom h-[100vh] max-h-[100vh]">
             <div class="container py-5 h-100">
                 <div class="row d-flex justify-content-center align-items-center h-100">
                     <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                         <div class="card bg-dark text-white" style={{ borderRadius: '1rem' }}>
                             <div class="card-body p-5 text-center">
-                                <div class="mb-md-5 mt-md-4 pb-5">
+                                <div class="">
                                     <Header/>
                                     <ListInputLogin/>
                                     <ListIcon/>
